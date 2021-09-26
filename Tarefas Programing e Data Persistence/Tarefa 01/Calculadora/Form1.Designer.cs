785a45609066a835594942644b03a57a71e612fa
Anderson <andersogui@gmail.com>
2021-09-26 20:03:23 -0300

    Calculadora WebForms

---
+++ Tarefas Programing e Data Persistence/Tarefa 01/Calculadora/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.Control;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(71, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(130, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 43);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.SystemColors.Control;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(130, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 43);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "6";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(71, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 43);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(12, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 43);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "4";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.Control;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(12, 58);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 43);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "7";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(71, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 43);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "8";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(130, 58);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 43);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "9";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.SystemColors.Control;
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Location = new System.Drawing.Point(12, 199);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 43);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "0";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.SystemColors.Control;
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Location = new System.Drawing.Point(130, 199);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 43);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "=";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.SystemColors.Control;
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Location = new System.Drawing.Point(189, 199);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(53, 43);
+            this.label12.TabIndex = 13;
+            this.label12.Text = "+";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.SystemColors.Control;
+            this.label13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label13.Location = new System.Drawing.Point(189, 58);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(53, 43);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "÷";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label13.Click += new System.EventHandler(this.label13_Click);
+            // 
+            // label14
+            // 
+            this.label14.BackColor = System.Drawing.SystemColors.Control;
+            this.label14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label14.Location = new System.Drawing.Point(189, 105);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(53, 43);
+            this.label14.TabIndex = 11;
+            this.label14.Text = "x";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.BackColor = System.Drawing.SystemColors.Control;
+            this.label15.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label15.Location = new System.Drawing.Point(189, 152);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(53, 43);
+            this.label15.TabIndex = 10;
+            this.label15.Text = "-";
+            this.label15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label15.Click += new System.EventHandler(this.label15_Click);
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.labelResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelResultado.Location = new System.Drawing.Point(12, 9);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(230, 43);
+            this.labelResultado.TabIndex = 14;
+            this.labelResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 259);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label labelResultado;
+    }
+}
+
---
+++ Tarefas Programing e Data Persistence/Tarefa 01/Calculadora/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public string Valor01 { get; set; }
+        public string Valor02 { get; set; }
+        public string Operacao { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void montarExibicao() {
+            var exibicao = string.Empty;
+
+            if (string.IsNullOrEmpty(Valor01))
+            {
+                exibicao = string.Empty;
+            }
+            else if (string.IsNullOrEmpty(Operacao)) {
+                exibicao = Valor01;
+            } else if (string.IsNullOrEmpty(Valor02)) {
+                exibicao = $"{Valor01} {Operacao}";
+            }
+            else {
+                exibicao = $"{Valor01} {Operacao} {Valor02}";
+            }
+
+            labelResultado.Text = exibicao;
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Valor01) && !string.IsNullOrEmpty(Operacao) && !string.IsNullOrEmpty(Valor02))
+            {
+                var valor01 = int.Parse(Valor01);
+                var valor02 = int.Parse(Valor02);
+                var resultado = 0;
+
+                if (Operacao == "+")
+                {
+                    resultado = valor01 + valor02;
+                }
+                else if (Operacao == "-")
+                {
+                    resultado = valor01 - valor02;
+                }
+                else if (Operacao == "*")
+                {
+                    resultado = valor01 * valor02;
+                }
+                else if (Operacao == "/")
+                {
+                    resultado = valor01 / valor02;
+                }
+
+                labelResultado.Text = $"{resultado}";
+                Valor01 = string.Empty;
+                Valor02 = string.Empty;
+                Operacao = string.Empty;
+            }
+        }
+
+        private void inserirOperacao(string operacao)
+        {
+            if (!string.IsNullOrEmpty(Valor01) && string.IsNullOrEmpty(Valor02) && string.IsNullOrEmpty(Operacao))
+            {
+                Operacao = operacao;
+                montarExibicao();
+            }
+        }
+
+        private void inserirValor(string valor)
+        {
+            if (string.IsNullOrEmpty(Operacao))
+            {
+                Valor01 += valor;
+            }
+            else
+            {
+                Valor02 += valor;
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Operacao))
+            {
+                if (string.IsNullOrEmpty(Valor01) == false)
+                {
+                    Valor01 += "0";
+                }
+            }
+            else {
+                if (string.IsNullOrEmpty(Valor02) == false)
+                {
+                    Valor02 += "0";
+                }
+            }
+
+            montarExibicao();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelResultado.Text = string.Empty;
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            inserirOperacao("+");
+            montarExibicao();
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            inserirOperacao("-");
+            montarExibicao();
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            inserirOperacao("*");
+            montarExibicao();
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            inserirOperacao("/");
+            montarExibicao();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            inserirValor("1");
+            montarExibicao();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            inserirValor("2");
+            montarExibicao();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            inserirValor("3");
+            montarExibicao();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            inserirValor("4");
+            montarExibicao();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            inserirValor("5");
+            montarExibicao();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            inserirValor("6");
+            montarExibicao();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            inserirValor("7");
+            montarExibicao();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            inserirValor("8");
+            montarExibicao();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            inserirValor("9");
+            montarExibicao();
+        }
+    }
+}
